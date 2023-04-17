fab30149c532650041d0a20d4a2dea01ae2921ca
fais1242 <farissamutsarun@gmail.com>
2023-04-17 15:13:40 +0700

    faiscomment

--- routes/company.js
+++ routes/company.js
@@ -1,27 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const db = require('../database/dbconfig.js');
+const db = require("../database/dbconfig.js");
+//faisfn
+router.get("/list", (req, res) => {
+  db.getConnection((err, connection) => {
+    if (err) throw err;
+    //console.log('connected as id  ',connection.threadId)
+    let qry =
+      "select * from company c,branch b where c.company_id=b.company_id";
+    let Js = {};
+    // let qry = 'SELECT * FROM company'
+    connection.query(qry, (err, rec) => {
+      //connection.release()
+      if (!err) {
+        res.json(rec);
+        // for (let row of rec.rows)
+        //{
 
-router.get('/list' , (req , res) => {
-    db.getConnection((err, connection) =>{
-    if (err) throw err
-        //console.log('connected as id  ',connection.threadId)
-        let qry = 'select * from company c,branch b where c.company_id=b.company_id'
-        let Js = {}
-       // let qry = 'SELECT * FROM company'
-        connection.query(qry ,(err,rec) => {
-        //connection.release()
-        if (!err){
-          res.json(rec)
-         // for (let row of rec.rows)
-          //{
-            
-         // }
-        }else{
-          res.send(err).status(400)
-        }
-    })
-    })
-})
+        // }
+      } else {
+        res.send(err).status(400);
+      }
+    });
+  });
+});
 
-  module.exports = router;
+module.exports = router;
