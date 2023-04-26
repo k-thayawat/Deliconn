a45a4625622ef5433ee4be5cff38d89bf53caccf
kanja95 <kan.thayawat@gmail.com>
2023-04-26 10:57:24 +0700

    dbconfigupdate

--- Database/dbconfig.js
+++ Database/dbconfig.js
@@ -4,9 +4,10 @@ const mysql = require('mysql');
 // create the connection to database
 const connection = mysql.createPool({
   connectionLimit : 10,
-  host: 'localhost',
-  user: 'root',
-  database: 'deliconn',
+  host: 'db4free.net',
+  user: 'malice',
+  password : 'malice12',
+  database: 'kanja_db'
 });
 
 module.exports = connection;
--- routes/company.js
+++ routes/company.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const db = require("../database/dbconfig.js");
+const db = require("../Database/dbconfig.js");
 
 router.get("/", (req, res) => {
   res.status(200).send(`Change path to companyadmin`)
